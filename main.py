cd6c1d56801c164d6f9d1bf03820981384182703
Jinu Choi <jinujinu99@gmail.com>
2025-08-13 11:58:27 +0900

    create mcp server

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("Calculator")
+
+@mcp.tool()
+def add(a: float, b: float) -> float:
+    """Add two numbers together."""
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run()
